0b4bff743664b0ef69dfb0ab6c23559dd824f52e
MATHEUSXDOK <164098328+MATHEUSXDOK@users.noreply.github.com>
2025-03-25 17:08:42 -0300

    Add files via upload

---
+++ ProjetoFujiokaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projeto_fujioka_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFujiokaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFujiokaSpringApplication.class, args);
+	}
+
+}
